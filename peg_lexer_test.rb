11840951ca1d8ab6533e92ca0c86c5b3ed968f03
hsssnow23 <shsnow23@gmail.com>
2016-08-23 05:21:52 +0900

    remove garbage of peg_lexer_test

--- peg_lexer_test.rb
+++ peg_lexer_test.rb
@@ -1,37 +1,7 @@
 
 require './peg_lexer'
 
-# indent = Indent.new()
-# puts(indent.block do
-#     add "if (true) {"
-#     indent.block do
-#         add "printf(\"Hello!!\\n\");"
-#     end
-#     add "}"
-# end)
-#
-# puts(indent.block do
-#     add "while (false) {"
-#     indent.block do
-#         add "printf(\"Hello!!\\n\");"
-#     end
-#     add "}"
-# end)
-#
-# lexer = Lexer.new()
-# parsed = lexer.ident.exec("abcd01")
-# puts parsed.type
-# puts parsed.value
-#
-# s_parsed = lexer.string.exec("L\"Hello!!\"")
-# puts s_parsed.type
-# puts s_parsed.value.prefix
-# puts s_parsed.value.s
-
 lexer = Lexer.new()
-
-# p lexer.variable.exec("int a = 1").value
-# p lexer.variable.exec("int a = 1")
 
 parsed = lexer.parse(File.read("example.c"))
 # p parsed.type
@@ -44,7 +14,6 @@ parsed = lexer.parse(File.read("example.c"))
 s = "\n"
 indent = Indent.new()
 for e in parsed.value
-    # p e.generate_src(indent)
     s += e.generate_src(indent) + "\n"
 end
 puts s
